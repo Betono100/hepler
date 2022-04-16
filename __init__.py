0b28a85013b6e4163f1c2b367ba369810178773a
neetqw <neetqw@gmail.com>
2022-04-16 08:18:28 +0700

    big edits6

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ login_manager = LoginManager(app)
 
 
 app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:Neet2001@localhost:3306/hepler?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:Neet2001@localhost:3306/helper?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
